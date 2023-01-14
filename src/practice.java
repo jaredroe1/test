47e1e843422fa8edc64b80fbfff2473655ac50a8
Jared Roe <jaredroe1@gmail.com>
2023-01-14 10:52:46 -0600

    Int convert to Array added

---
+++ src/practice.java
@@ -0,0 +1,62 @@
+public class practice {
+
+    //static int sumOfDigits(int digits) {
+        //String temp = Integer.toString(digits);
+       // int[] numbers = new int[temp.length()];
+       // for (int i = 0; i < temp.length(); i++) {
+            //numbers[i] = Character.getNumericValue(temp.charAt(i));
+        //}
+        //int sum = 0;
+        //for (int number: numbers){
+           // sum += number;
+       // }
+        //return sum;
+   // }
+
+
+
+    public static void main(String[] args) {
+
+
+        // int[] array = {1, 5, 2, 8, 13, 6};
+        //System.out.println(array[0]);
+        //System.out.println(array[array.length - 1]);
+        //for (int i = 0; i < array.length; i++){
+        // System.out.println(array[i]);
+        // }
+
+        //for (int number : array) {
+        //System.out.println(number);
+        // }
+
+        //double sum = 0;
+        //for (int number : array) {
+        //sum += number;
+        //}
+        //System.out.println(sum);
+
+        //double average = sum / array.length;
+        //System.out.println(average);
+
+        //for (int number : array) {
+        //if (number % 2 == 1) {
+        //System.out.println(number);
+        //}
+        //}
+
+        //String[] names = {"Sam", "Sally", "Thomas", "Robert"};
+        // int sumOfLetters = 0;
+        //for (String name : names) {
+        // sumOfLetters += name.length();
+
+        //}
+        //System.out.println(sumOfLetters);
+
+        //int digits = 12;
+        //int sumAll = sumOfDigits(digits);
+        //System.out.println("The sum of all digits for the value " + digits + " is " + sumAll + ".");
+
+
+
+    }
+}
