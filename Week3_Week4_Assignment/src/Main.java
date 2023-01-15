aead1ab4a3fc158f209b050161c3bfed5a0d865c
Jared Roe <jaredroe1@gmail.com>
2023-01-15 08:02:44 -0600

    Week 3 Week 4 Assignment first push, questions 2, 3, and 4 answers added

--- Week3_Week4_Assignment/src/Main.java
+++ Week3_Week4_Assignment/src/Main.java
@@ -2,6 +2,8 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+
+
         int[] ages = {3, 9, 23, 64, 2, 8, 28, 93, 105};
         int lastMinusFirst = ages[ages.length - 1] - ages[0];
         System.out.println(lastMinusFirst);
@@ -33,11 +35,40 @@ public class Main {
         //System.out.println(joined);
         // this seemed to get the desired result but did not use a loop to join the names together
 
-        //to access the last element of any array
+        //to access the last element of any array use the name of the array[arrayname.length-1]
         System.out.println(names[names.length-1]);
 
-        // to access the first element of any array
+        // to access the first element of any array us the name of the array with first index array[0]
         System.out.println(names[0]);
+
+        //Create a new array of int called nameLengths. Write a loop to iterate over the previously created names array and add the length of each name to the nameLengths array.
+
+        //Write a loop to iterate over the nameLengths array and calculate the sum of all the elements in the array. Print the result to the console.
+
+
+        System.out.println(multiplyString("Welcome", 3));
+
+        System.out.println(fullName("Jared", "Roe"));
+
+
+    }
+
+
+
+
+    //Write a method that takes a String, word, and an int, n, as arguments and returns the word concatenated to itself n number of times. (i.e. if I pass in “Hello” and 3, I expect the method to return “HelloHelloHello”).
+        public static String multiplyString(String str, int num){
+            String result = "";
+            for (int i = 0; i < num; i++){
+                result += str;
+            }
+            return result;
+
+        }
+
+        //Write a method that takes two Strings, firstName and lastName, and returns a full name (the full name should be the first and the last name as a String separated by a space).
+        public static String fullName(String firstName, String lastName){
+            return firstName + " " + lastName;
         }
     }
 
